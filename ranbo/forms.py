07cd12d65d367fe543d842dacba6a8a1968940dc
Ceyu Gao <2556539G@student.gla.ac.uk>
2021-08-06 21:39:52 +0800

    resolve conflict

--- ranbo/forms.py
+++ ranbo/forms.py
@@ -1,35 +1,40 @@
 from django import forms
-#from ranbo.models import Page, Category
 from django.contrib.auth.models import User
 from ranbo.models import User, Post, like
 from ranbo.models import UserProfile
 
+
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
-    User_id = forms.IntegerField()
+    User_id = forms.IntegerField(widget=forms.HiddenInput())
     Username = forms.CharField(max_length=128)
+
     class Meta:
         model = User
-        fields = ('User_id','Username', 'password',)
+        fields = ('username', 'password',)
 
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('website', 'picture',) 
-        
+        fields = ('website', 'picture',)
+
+
 class PostForm(forms.ModelForm):
     Post_id = forms.IntegerField()
     View_times = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    Like_times =forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    Like_times = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     Content = forms.TextInput()
     Picture = forms.ImageField(help_text="Please enter the picture.")
+
     class Meta:
         model = Post
-        fields = ('Post_id','Content', 'Picture',)
+        fields = ('post_id', 'content', 'picture',)
 
-class likeForm(forms.ModelForm):
+
+class LikeForm(forms.ModelForm):
     like_id = forms.IntegerField()
+
     class Meta:
         model = like
         fields = ('like_id',)
